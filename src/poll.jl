b05bd7b1ef7e35c27a0253214333aa0cb346b7dc
notinaboat <notinaboat@icloud.com>
2021-07-13 12:32:22 +1000

    remove debug io_monitor

--- src/poll.jl
+++ src/poll.jl
@@ -18,16 +18,23 @@ end
 
 Base.show(io::IO, fd::PollFD) = print(io, "PollFD($(fd.fd), $(fd.events))")
 
+
 """
 Global queue of file descriptors to poll.
 """
 mutable struct PollQueue
+    channel::Channel{PollFD}
     queue::Vector{PollFD}
     vector::Vector{PollFDTuple}
-    lock::Threads.SpinLock
+    task::Union{Nothing,Task}
 end
 
-const poll_queue = PollQueue([], [], Threads.SpinLock())
+
+const poll_queue = PollQueue(Channel{PollFD}(0),[], [], nothing)
+
+function __init__()
+    poll_queue.task = @async poll_task(q)
+end
 
 
 """
@@ -38,27 +45,9 @@ See [poll(2)](https://man7.org/linux/man-pages/man2/poll.2.html)
 for event types.
 """
 function poll_wait(fd::PolledUnixFD, events)
-    global poll_queue
-    q = poll_queue
-
     lock(fd.ready)
     try
-        # Insert a new `PollFD` in the global `poll_queue`.
-        lock(q.lock)
-        isfirst = isempty(q.queue)
-        isdupe = findfirst(x->x.fd == fd.fd, q.queue) != nothing
-        push!(q.queue, PollFD(fd.fd, events, fd.ready))
-        unlock(q.lock)
-
-        @assert !isdupe # FIXME reconsider if/when poll is used
-                        #       for writes as well as reads.
-
-        # Start the polling task if needed.
-        if isfirst == 1
-            @async poll_task(q)
-        end
-
-        # Wait...
+        push!(poll_queue.channel, PollFD(fd.fd, events, fd.ready))
         wait(fd.ready)
     finally
         unlock(fd.ready)
@@ -72,12 +61,22 @@ Run `poll(queue)` while there are entries in the `queue`.
 function poll_task(q)
     while true
         try
-            poll(q) || return
+            printerr("poll_task")
+            while isready(q.channel) || isempty(q.queue)
+                fd = take!(q.channel)
+                @assert findfirst(x->x.fd == fd.fd, q.queue) == nothing
+                                        # FIXME reconsider if/when poll is used
+                                        #       for writes as well as reads.
+                push!(q.queue, fd)
+            end
+
+            printerr("poll_task poll")
+            poll(q)
+
         catch err
             exception=(err, catch_backtrace())
             @error "Error in poll_task()" exception
         end
-        sleep(0.01)
     end
 end
 
@@ -89,18 +88,16 @@ Return false if the queue is empty.
 """
 function poll(q::PollQueue)
 
-    # Build vector of `struct pollfd`.
-    lock(q.lock)
     queue_length = length(q.queue)
     if queue_length == 0
-        unlock(q.lock)
-        return false
+        return
     end
+
+    # Build vector of `struct pollfd`.
     resize!(q.vector, queue_length)
     for (i, fd) in enumerate(q.queue)
         q.vector[i] = (fd.fd, fd.events, 0)
     end
-    unlock(q.lock)
 
     # Wait for events
     timeout_ms = 10
@@ -111,25 +108,21 @@ function poll(q::PollQueue)
 
     # Check poll vector for events.
     for (fd, events, revents) in q.vector
-        if revents == 0
-            continue
+        if revents != 0
+
+            # Remove from queue.
+            i = findfirst(x->x.fd == fd #=&& x.events == events=#, q.queue)
+            ready = q.queue[i].ready    # ^^ FIXME reconsider if poll is used
+            deleteat!(q.queue, i)       #          for writes as well as reads.
+
+            # Notify waiting task.
+            lock(ready)
+            notify(ready)
+            unlock(ready)
         end
-
-        # Remove from queue.
-        lock(q.lock)
-        i = findfirst(x->x.fd == fd #=&& x.events == events=#, q.queue)
-        ready = q.queue[i].ready     # ^^ FIXME reconsider if/when poll is used
-        deleteat!(q.queue, i)        #          for writes as well as reads.
-        queue_length = length(q.queue)
-        unlock(q.lock)
-
-        # Notify waiting task.
-        lock(ready)
-        notify(ready)
-        unlock(ready)
     end
 
-    return queue_length > 0
+    nothing
 end
 
 
