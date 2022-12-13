311c879d383989c37183ae082803022e99796283
notinaboat <notinaboat@icloud.com>
2022-12-13 12:24:30 +1100

    more slowfs_tests.jl

--- test/slowfs_tests.jl
+++ test/slowfs_tests.jl
@@ -27,15 +27,22 @@ end
         @time @sync for f in ["A", "B", "C", "D", "E", "F"]
             @async begin
                 io = UnixIO.open("slowfs_mount/$f")
+                if x == 2 && f == "A"
+                    @show typeof(io.in.stream)
+                    @show UnixIO.IOTraits.WaitingMechanism(io.in.stream)
+                    @show UnixIO.IOTraits.TransferMode(io.in.stream)
+                    @show UnixIO.IOTraits.TransferMechanism(io.in.stream)
+                end
                 try
-                    @show read(io, String)
+                    x == 1 || @show read(io, String)
                 finally
                     close(io)
                 end
             end
+            x == 2 || break
         end
     end
-    @test time() - t0 < 3
+    @test time() - t0 < 4
 
 end #testset
 
@@ -48,12 +55,47 @@ end #testset
             @async begin
                 io = UnixIO.open("slowfs_mount/$f";
                                  transfer_mode=UnixIO.IOTraits.ImmediateTransfer)
+                if x == 2 && f == "A"
+                    @show typeof(io.in.stream)
+                    @show UnixIO.IOTraits.WaitingMechanism(io.in.stream)
+                    @show UnixIO.IOTraits.TransferMode(io.in.stream)
+                    @show UnixIO.IOTraits.TransferMechanism(io.in.stream)
+                end
                 try
-                    @show read(io, String)
+                    x == 1 || @show read(io, String)
                 finally
                     close(io)
                 end
             end
+            x == 2 || break
+        end
+    end
+    @test time() - t0 > 6
+
+end #testset
+
+@testset LoggingTestSet "Slow FS - BlockingTransfer" begin
+
+    t0 = nothing
+    for x in 1:2
+        t0 = time()
+        @time @sync for f in ["A", "B", "C", "D", "E", "F"]
+            @async begin
+                io = UnixIO.open("slowfs_mount/$f";
+                                 transfer_mode=UnixIO.IOTraits.BlockingTransfer)
+                if x == 2 && f == "A"
+                    @show typeof(io.in.stream)
+                    @show UnixIO.IOTraits.WaitingMechanism(io.in.stream)
+                    @show UnixIO.IOTraits.TransferMode(io.in.stream)
+                    @show UnixIO.IOTraits.TransferMechanism(io.in.stream)
+                end
+                try
+                    x == 1 || @show read(io, String)
+                finally
+                    close(io)
+                end
+            end
+            x == 2 || break
         end
     end
     @test time() - t0 > 6
